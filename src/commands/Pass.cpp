4f65bec14e4e8edd531ffa4c55df164e4c25d5f8
Mohamed El-Houary <mohamedelhouary056@gmail.com>
2026-07-12 15:49:18 +0100

    fix unsed param

--- src/commands/Pass.cpp
+++ src/commands/Pass.cpp
@@ -2,10 +2,10 @@
 
 
 
-// void handlePass(Client &client, IRCMessage &msg, Server &server)
-// {
-//     void (*client);
-//     void (*msg);
-//     void (*ClientPassorwd);
+void handlePass(Client &client, IRCMessage &msg, Server &server)
+{
+    void (*client);
+    void (*msg);
+    void (*ClientPassorwd);
 
-// } 
+} 
