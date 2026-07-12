42f430294b4a16e730f76a76719ce2f9c5a158fb
Mohamed El-Houary <mohamedelhouary056@gmail.com>
2026-07-12 15:40:11 +0100

    compete parssing && add getpassword n server && implement pass fucntion

--- src/commands/Pass.cpp
+++ src/commands/Pass.cpp
@@ -5,5 +5,8 @@
 void handlePass(Client &client, IRCMessage &msg, Server &server)
 {
     std::string ClientPassorwd =  server.getPassword();
-    
+    void (*client);
+    void (*msg);
+    void (*ClientPassorwd);
+
 } 
