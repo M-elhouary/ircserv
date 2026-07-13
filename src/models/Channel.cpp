6ab2970c129c62761562eee00e5efa8a0656ab83
Mohamed El-Houary <mohamedelhouary056@gmail.com>
2026-07-13 20:06:21 +0100

    fix unesed param

--- src/models/Channel.cpp
+++ src/models/Channel.cpp
@@ -14,30 +14,34 @@ std::string Channel::getName() const
 
 void Channel::addClient(Client *client)
 {
-
+    (void)client;
 };
 
 void Channel::removeClient(Client *client)
 {
-
+    (void)client;
 };
+
 
 bool Channel::isClientInChannel(Client *client) const
 {
-
+    (void)client;
+    return false;
 };
 
 void Channel::addOperator(const std::string &nickname)
 {
-
+    (void)nickname;
 };
 
 void Channel::removeOperator(const std::string &nickname)
 {
-
+    (void)nickname;
 };
+
 
 bool Channel::isOperator(const std::string &nickname) const
 {
-    
+    (void)nickname;
+    return false;   
 };
