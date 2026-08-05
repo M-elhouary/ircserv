dcb6d2ea81ab3aaf0052caaa148fe132fbe712e2
røsus <190874852+rosuz@users.noreply.github.com>
2026-08-05 03:23:02 +0100

    refactor(server): replace sigaction with signal() for signal handling

--- src/server/ServerInit.cpp
+++ src/server/ServerInit.cpp
@@ -8,17 +8,10 @@ void handleShutdownSignal(int) {
 }
 
 void Server::init() {
-    struct sigaction sa;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-
-    sa.sa_handler = handleShutdownSignal;
-    sigaction(SIGINT, &sa, NULL);
-    sigaction(SIGTERM, &sa, NULL);
-    sigaction(SIGQUIT, &sa, NULL);
-
-    sa.sa_handler = SIG_IGN;
-    sigaction(SIGPIPE, &sa, NULL);
+    signal(SIGINT, handleShutdownSignal);
+    signal(SIGTERM, handleShutdownSignal);
+    signal(SIGQUIT, handleShutdownSignal);
+    signal(SIGPIPE, SIG_IGN);
 
     setupSocket();
     running = true;
